352b85eb251a30916e58e86f33c6eb657001ed29
Aman Singhal <amansinghal284@gmail.com>
2020-05-13 01:16:18 +0530

    completed React

---
+++ public/precache-manifest.835e8d2fd9b2aa899dc5c3f266732fde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab28824b6c18f1c9533b18c3f7cf3e3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3312c8e4bd6a1165d234",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "adea4755a5e110a2f7d0",
+    "url": "/static/js/2.17b96468.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.17b96468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3312c8e4bd6a1165d234",
+    "url": "/static/js/main.f087e89c.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
